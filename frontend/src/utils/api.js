52e62a2b976597becd7d6df475cc4d6968a8a56c
meashishpatel <ashishpatel3009@gmail.com>
2025-01-02 19:39:04 +0530

    Refactor axios configuration by updating base URL and removing unused fetch example

--- frontend/src/utils/api.js
+++
@@ -1,7 +0,0 @@
-// Example fetch call
-fetch("https://calendar-application-for-communication-16bb.onrender.com", {
-  method: "GET",
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
--- frontend/src/utils/axiosConfig.js
+++ frontend/src/utils/axiosConfig.js
@@ -2,8 +2,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL:
-    "https://calendar-application-for-communication-16bb.onrender.com/api", // Update to your backend base URL
+  baseURL: "https://calendar-application-for-communication-16bb.onrender.com", // Update to your backend base URL
 });
 
 export default axiosInstance;
